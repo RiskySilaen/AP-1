f2ca20b57df94d0d60f060120a6add86f60f474b
Risky Silaen <riskyjonalson@students.usu.ac.id>
2025-05-25 21:26:15 +0700

    Update belajarfugsi.cpp

--- PERTEMUAN 6/belajarfugsi.cpp
+++ PERTEMUAN 6/belajarfugsi.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama) {
+void sapa(string nama) { // untuk menerima nama bertipe data string
     cout << "Halo " << nama << "! Selamat belajar C++!" << endl;
 }
 
 int main () {
-    string namapengguna = "Alya";
+    string namapengguna = "Alya"; // digunakan untuk mendeklarasikan sebuah variabel bertipe string
 
-    sapa(namapengguna);
+    sapa(namapengguna); // memanggil fungsi sapa
 
     return 0;
 }
