66991f4942653eeab659cb8aa4a7fbb8c19de50c
Risky Silaen <riskyjonalson@students.usu.ac.id>
2025-05-25 21:19:44 +0700

    Update latihan.cpp

--- PERTEMUAN 3/latihan.cpp
+++ PERTEMUAN 3/latihan.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 int main ()
 {
-    system("CLS");
+    system("CLS"); // membersihkan terminal
 
     int nilai;
     cout << "masukkan nilai: ";
     cin >> nilai;
+    // Memeriksa apakah 'nilai' adalah kelipatan 10
     if (nilai % 10 == 0)
     {
         cout << "nilai adalah kelipatan 10";
     }
+ // Memeriksa apakah 'nilai' adalah kelipatan 5
     else if (nilai % 5 == 0)
     {
         cout << "nilai adalah kelipatan 5";
     }
     else
     {
-        cout << "nilai bukan kelipatan dari keduanya";
+        cout << "nilai bukan kelipatan dari keduanya"; // // Menampilkan pesan jika 'nilai' bukan kelipatan dari 5 atau 10
     }
 }
