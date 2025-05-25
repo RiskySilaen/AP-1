3b6617f0f5913d070bd0843d4ce4d3e2cd6c5f7e
Risky Silaen <riskyjonalson@students.usu.ac.id>
2025-05-25 21:16:32 +0700

    Update belajar.cpp

--- PERTEMUAN 1/belajar.cpp
+++ PERTEMUAN 1/belajar.cpp
@@ -1,3 +1,41 @@
+#include <iostream> // Header untuk input/output
+#include <conio.h>  // Header untuk fungsi konsol (seperti getche dan getch)
+
+using namespace std; // Menggunakan namespace std agar tidak perlu menulis std sebelum setiap fungsi
+
+int main() {
+    string nama;          // Variabel untuk menyimpan nama (tipe string)
+    char kom, jeniskelamin; // Variabel untuk menyimpan komponen dan jenis kelamin (tipe char)
+    int nim;              // Variabel untuk menyimpan Nomor Induk Mahasiswa (NIM) (tipe integer)
+    float ip;             // Variabel untuk menyimpan Indeks Prestasi (IP) (tipe float)
+
+    cout << "Hello world!"; // Menampilkan pesan "Hello world!" ke layar
+    cout << "masukkan nama: "; // Meminta pengguna untuk memasukkan nama
+    getline(cin, nama); // Mengambil input string untuk nama, termasuk spasi
+
+    cout << "masukkan kom: "; // Meminta pengguna untuk memasukkan komponen
+    cin >> kom; // Mengambil input karakter untuk komponen
+
+    cout << "masukkan nim: "; // Meminta pengguna untuk memasukkan NIM
+    cin >> nim; // Mengambil input integer untuk NIM
+
+    cout << "masukkan ip: "; // Meminta pengguna untuk memasukkan IP
+    cin >> ip; // Mengambil input float untuk IP
+
+    cout << "masukkan jenis kelamin (L/P): "; // Meminta pengguna untuk memasukkan jenis kelamin
+    jeniskelamin = getche(); // Mengambil input karakter untuk jenis kelamin dan langsung menampilkannya tanpa perlu menekan enter
+
+    /* masukkan output */
+    cout << "\n" << nama << endl; // Menampilkan nama yang dimasukkan
+    cout << kom << endl; // Menampilkan komponen yang dimasukkan
+    cout << nim << endl; // Menampilkan NIM yang dimasukkan
+    cout << ip << endl; // Menampilkan IP yang dimasukkan
+    putchar(jeniskelamin); // Menampilkan karakter jenis kelamin yang dimasukkan
+
+    getch(); // Menunggu pengguna menekan tombol sebelum program berakhir (karakter yang diketik tidak ditampilkan)
+
+    return 0; // Mengembalikan 0 untuk menandakan bahwa program selesai dengan sukses
+}
 #include <iostream> // header untuk cpp
 #include <conio.h> //header untuk getche
 
