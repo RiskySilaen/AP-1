9d6b7b6709af30c4a048d5771d7b96a838bdc092
Risky Silaen <riskyjonalson@students.usu.ac.id>
2025-05-25 21:22:35 +0700

    Update latihan1.cpp

--- PERTEMUAN 4/latihan1.cpp
+++ PERTEMUAN 4/latihan1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    string kalimat;
-    int i;
+    string kalimat; // variabel kalimat bertipe string
+    int i; // variabel i bertipe integer
 
     cout << "masukkan kalimat: ";
-    getline(cin, kalimat);
+    getline(cin, kalimat); // biar bisa membaca spasi
 
     for(i = 0; i < kalimat.length(); i++){
         kalimat[i] = toupper(kalimat[i]);
@@ -14,5 +14,5 @@ int main(){
 
     cout<< "kalimat dalam huruf kapital: " << kalimat << endl;
 
-    return 0;
+    return 0; 
 }
